2958b82ac19388df37aa2b269ed9777d8725922c
Cesar Silva <84938508+cfsilva28@users.noreply.github.com>
2021-11-06 11:14:56 -0300

    Teste Unitário Com WireMock

---
+++ src/test/java/testeApiWireMock/TesteApiWireMock.java
@@ -0,0 +1,37 @@
+package testeApiWireMock;
+
+import org.apache.http.impl.conn.Wire;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import io.restassured.RestAssured;
+
+public class TesteApiWireMock {
+
+	@Rule
+	public WireMockRule wireMockRule = new WireMockRule(8089);
+	
+
+	@Test
+	public void testWithWireMock() {
+		stubFor(post("/api/teste")
+				.withHeader("Content-Type", containing("json"))
+				.willReturn(
+				ok()
+				.withHeader("Content-Type", "application/json")
+				.withBody("Usuario cadastrado com sucesso!")));
+
+		RestAssured.given()
+		.header("Content-Type", "application/json")
+		.when()
+		.post("http://localhost:8089/api/teste") 
+				.then()
+				.assertThat().log().all()
+				.body(Matchers.equalTo("Usuario cadastrado com sucesso!"));
+
+}
+	}
